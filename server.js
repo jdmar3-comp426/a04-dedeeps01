576a7dca6bc269edc716dc38d5c2bd608cfd66fd
Dedeepya Madineni <dedeepya01@yahoo.com>
2021-12-04 21:56:36 -0500

    delete fix

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ app.get("/app/", (req, res, next) => {
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post( "/app/new/", ( req, res ) => {
+app.post("/app/new/", ( req, res ) => {
 	const stmt = db.prepare(
-	  `INSERT INTO userInfo ( user, pass )
-	  VALUES ( ?, ? );`
+	  `INSERT INTO userInfo (user, pass)
+	  VALUES (?, ?);`
 	);
 	const info = stmt.run( req.body.user, md5( req.body.pass ) );
     res.status(201).json( { "message" : info.changes + " record created: ID " + info.lastInsertRowid + " (201)" } );
@@ -39,31 +39,31 @@ app.get("/app/users/", (req, res) => {
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get( "/app/user/:id", ( req, res ) => {
+app.get("/app/user/:id", (req, res) => {
 	const stmt = db.prepare(
 	  `SELECT * FROM userInfo WHERE id = ?`
 	);
-	const user = stmt.get( req.params.id );
+	const user = stmt.get(req.params.id);
 	res.json(user);
 } );
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.patch( "/app/update/user/:id", ( req, res ) => {
+app.patch("/app/update/user/:id", ( req, res ) => {
 	const stmt = db.prepare(
 	  `UPDATE userInfo
 	  SET user = COALESCE( ?, user ), pass = COALESCE( ?, pass )
 	  WHERE id = ?`
 	);
-	let info = stmt.run( req.body.user, md5(req.body.pass), req.body.id );
+	const info = stmt.run( req.body.user, md5(req.body.pass), req.body.id );
 	res.status(200).json( { "message" : info.changes + " record updated: ID " + req.body.id + " (200)" } );
 });
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.delete( "/app/delete/user/:id", (req, res) => {
+app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare(
 	  `DELETE FROM userInfo WHERE id = ?`
 	);
-	const info = stmt.run( req.body.id );
+	const info = stmt.run(req.body.user, md5(req.body.pass), req.body.id );
 	res.status(200).json( { "message" : info.changes + " record deleted: ID " + req.body.id + " (200)" } );
 });
 
