c62b3c0e6a5ff23dd3919ab63b98d9642ed6e3b5
Dedeepya Madineni <dedeepya01@yahoo.com>
2021-12-04 22:14:31 -0500

    change body to params

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.post("/app/new/", ( req, res ) => {
 	  `INSERT INTO userInfo (user, pass)
 	  VALUES (?, ?);`
 	);
-	const info = stmt.run( req.body.user, md5( req.body.pass ) );
+	const info = stmt.run(req.body.user, md5( req.body.pass ));
     res.status(201).json( { "message" : info.changes + " record created: ID " + info.lastInsertRowid + " (201)" } );
 });
 
@@ -48,13 +48,13 @@ app.get("/app/user/:id", (req, res) => {
 } );
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.patch("/app/update/user/:id", ( req, res ) => {
+app.patch("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare(
 	  `UPDATE userInfo
-	  SET user = COALESCE( ?, user ), pass = COALESCE( ?, pass )
+	  SET user = COALESCE(?, user), pass = COALESCE(?, pass)
 	  WHERE id = ?`
 	);
-	const info = stmt.run( req.body.user, md5(req.body.pass), req.body.id );
+	const info = stmt.run( req.body.user, md5(req.body.pass), req.params.id );
 	res.status(200).json( { "message" : info.changes + " record updated: ID " + req.body.id + " (200)" } );
 });
 
@@ -63,7 +63,7 @@ app.delete("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare(
 	  `DELETE FROM userInfo WHERE id = ?`
 	);
-	const info = stmt.run(req.body.user, md5(req.body.pass), req.body.id );
+	const info = stmt.run(req.params.id);
 	res.status(200).json( { "message" : info.changes + " record deleted: ID " + req.body.id + " (200)" } );
 });
 
