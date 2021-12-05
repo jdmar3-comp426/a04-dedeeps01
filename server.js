953455c2930b9c62a30ef8828aeef0fd81cb38bb
Dedeepya Madineni <dedeepya01@yahoo.com>
2021-12-04 21:26:06 -0500

    fixed naming issue

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.listen(HTTP_PORT, () => {
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API is working! (200)"});
+    res.json({"message":"Your API works! (200)"});
 	res.status(200);
 });
 
@@ -41,7 +41,7 @@ app.get("/app/users", (req, res) => {
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get( "/app/user/:id", ( req, res ) => {
 	const stmt = db.prepare(
-	  `SELECT * FROM userInfo WHERE userId = ?`
+	  `SELECT * FROM userInfo WHERE id = ?`
 	);
 	const user = stmt.get( req.params.id );
 	res.json(user);
@@ -52,7 +52,7 @@ app.patch( "/app/update/user/:id", ( req, res ) => {
 	const stmt = db.prepare(
 	  `UPDATE userInfo
 	  SET username = COALESCE( ?, username ), pass = COALESCE( ?, pass )
-	  WHERE userId = ?`
+	  WHERE id = ?`
 	);
 	let info = stmt.run( req.body.username, md5(req.body.pass), req.body.id );
 	res.status(200).json( { "message" : info.changes + " record updated: ID " + req.body.id + " (200)" } );
@@ -61,7 +61,7 @@ app.patch( "/app/update/user/:id", ( req, res ) => {
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete( "/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare(
-	  `DELETE FROM userInfo WHERE userId = ?`
+	  `DELETE FROM userInfo WHERE id = ?`
 	);
 	const info = stmt.run( req.body.userId );
 	res.status(200).json( { "message" : info.changes + " record deleted: ID " + req.body.d + " (200)" } );
